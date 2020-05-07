dc033e1683fc79cad55a148eba871ede559d9541
Mukesh Maurya <mukesh@insider.in>
2020-05-07 16:33:23 +0530

    added zeplin config

--- src/components/Button.stories.js
+++ src/components/Button.stories.js
@@ -1,15 +1,16 @@
-import React from 'react';
-import '../../src/index.css';
+import React from "react";
+import "../../src/index.css";
 // import { action } from '@storybook/addon-actions';
 
-import Button from './Button';
+import Button from "./Button";
 
 export default {
-    component: Button,
-    title: 'Button',
-    excludeStories: /.*Data$/,
-}
+  component: Button,
+  title: "Button",
+  excludeStories: /.*Data$/,
+};
 
-export const Default = () => <Button buttonState="default" />
-export const Hovered = () => <Button buttonState="hovered" />
-export const Clicked = () => <Button buttonState="clicked" />
+export const Default = () => <Button buttonState="default" />;
+export const Hovered = () => <Button buttonState="hovered" />;
+export const Clicked = () => <Button buttonState="clicked" />;
+export const Primary = () => <Button buttonState="primary" />;
